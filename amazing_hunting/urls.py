9d709c777c30e710c822623af41e2bf4beeba02e
Dmitriy Lahmenev <lahmenev@gmail.com>
2022-07-05 12:23:15 +0300

    End

--- amazing_hunting/urls.py
+++ amazing_hunting/urls.py
@@ -20,4 +20,7 @@ from vacancies import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', views.hello),
+    path('vacancy/', views.index),
+    path('vacancy/<int:vacancy_id>/', views.get),
+
 ]
