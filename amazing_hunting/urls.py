35698c9350cb13667c6a994d7b85fcd2e5109b4a
dmitrii_lakhmenev <lahmenev@gmail.com>
2022-07-20 22:23:04 +0300

    ViewSet

--- amazing_hunting/urls.py
+++ amazing_hunting/urls.py
@@ -17,8 +17,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
 
 from vacancies import views
+from vacancies.views import SkillsViewSet
+
+router = routers.SimpleRouter()
+router.register(r'skill', SkillsViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +33,8 @@ urlpatterns = [
     path('vacancy/', include('vacancies.urls')),
     path('company/', include('companies.urls')),
 ]
+
+urlpatterns += router.urls
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
