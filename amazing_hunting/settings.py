01f379c9dc3de7a0724f40f748584e8cdf0da085
Dmitriy Lahmenev <lahmenev@gmail.com>
2022-07-25 14:49:34 +0300

    JWT

--- amazing_hunting/settings.py
+++ amazing_hunting/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
     'vacancies',
     'companies',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -166,4 +169,10 @@ TOTAL_ON_PAGE = 10
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ]
 }
+
+AUTH_USER_MODEL = 'authentication.User'
